5019121db2405b04a61866b7aac3561a6f595997
Martin Ovesny <zitmen@gmail.com>
2013-03-13 19:00:08 +0100

    Detection + fitting work together.

--- src/ThunderSTORM/Thunder_STORM.java
+++ src/ThunderSTORM/Thunder_STORM.java
@@ -26,35 +26,30 @@ public final class Thunder_STORM {
         @Override
         public double getY(double x[], double[] a) {
             // a = {x0,y0,Intensity,sigma,background}
-            return a[2] / 2.0 / Math.PI / sqr(a[3]) * Math.exp(-(sqr(x[0] - a[0]) + sqr(x[1] - a[1])) / 2.0 / sqr(a[3])) + a[4];
+            return a[2]/2.0/Math.PI/sqr(a[3]) * Math.exp(-(sqr(x[0]-a[0]) + sqr(x[1]-a[1])) / 2.0 / sqr(a[3])) + a[4];
         }
 
         @Override
         public double getPartialDerivate(double x[], double[] a, int parameterIndex) {
             double arg = sqr(x[0] - a[0]) + sqr(x[1] - a[1]);
             switch (parameterIndex) {
-                case 0:
-                    return a[2] / 2.0 / Math.PI / Math.pow(a[3], 4) * (x[0] - a[0]) * Math.exp(-arg / 2.0 / sqr(a[3])); // x0
-                case 1:
-                    return a[2] / 2.0 / Math.PI / Math.pow(a[3], 4) * (x[1] - a[1]) * Math.exp(-arg / 2.0 / sqr(a[3])); // y0
-                case 2:
-                    return Math.exp(-arg / 2.0 / sqr(a[3])) / 2.0 / Math.PI / sqr(a[3]); // Intensity
-                case 3:
-                    return a[2] / 2.0 / Math.PI / Math.pow(a[3], 5) * (arg - 2.0 * sqr(a[3])) * Math.exp(-arg / 2.0 / sqr(a[3])); // sigma
-                case 4:
-                    return 1.0; // background
+                case 0: return a[2]/2.0/Math.PI/Math.pow(a[3],4) * (x[0]-a[0]) * Math.exp(-arg/2.0/sqr(a[3])); // x0
+                case 1: return a[2]/2.0/Math.PI/Math.pow(a[3],4) * (x[1]-a[1]) * Math.exp(-arg/2.0/sqr(a[3])); // y0
+                case 2: return Math.exp(-arg/2.0/sqr(a[3])) / 2.0 / Math.PI / sqr(a[3]); // Intensity
+                case 3: return a[2]/2.0/Math.PI/Math.pow(a[3],5) * (arg - 2.0 * sqr(a[3])) * Math.exp(-arg/2.0/sqr(a[3])); // sigma
+                case 4: return 1.0; // background
             }
             throw new RuntimeException("No such parameter index: " + parameterIndex);
         }
     }
 
-    public static Vector<Point<Integer>> WaveletDetector(FloatProcessor image, boolean third_plane, boolean watershed, boolean upsample) throws UnexpectedException {
+    public static Vector<Point> WaveletDetector(FloatProcessor image, boolean third_plane, boolean watershed, boolean upsample) throws UnexpectedException {
         assert (!((upsample == true) && (watershed == false))) : "Upsampling can be performed only along with watershed transform!";
 
         // wavelets definition
-        float[] g1 = new float[]{1f / 16f, 1f / 4f, 3f / 8f, 1f / 4f, 1f / 16f};
-        float[] g2 = new float[]{1f / 16f, 0f, 1f / 4f, 0f, 3f / 8f, 0f, 1f / 4f, 0f, 1f / 16f};
-        float[] g3 = new float[]{1f / 16f, 0f, 0f, 0f, 1f / 4f, 0f, 0f, 0f, 3f / 8f, 0f, 0f, 0f, 1f / 4f, 0f, 0f, 0f, 1f / 16f};
+        float[] g1 = new float[]{1f/16f, 1f/4f, 3f/8f, 1f/4f, 1f/16f};
+        float[] g2 = new float[]{1f/16f, 0f, 1f/4f, 0f, 3f/8f, 0f, 1f/4f, 0f, 1f/16f};
+        float[] g3 = new float[]{1f/16f, 0f, 0f, 0f, 1f/4f, 0f, 0f, 0f, 3f/8f, 0f, 0f, 0f, 1f/4f, 0f, 0f, 0f, 1f/16f};
 
         // prepare the wavelets for convolution
         FloatProcessor k1 = Convolution.getSeparableKernelFromVectors(g1, g1);
@@ -93,45 +88,58 @@ public final class Thunder_STORM {
         }
 
         // Detection - finding a center of gravity (with subpixel precision),
-        //   but since all other detectors return positions with pixel precision,
-        //   round the positions to pixels (integer coordinates)
-        Vector<Point<Integer>> detections = new Vector<>();
+        Vector<Point> detections = new Vector<>();
         for (Graph.ConnectedComponent c : Graph.getConnectedComponents((ImageProcessor) final_plane, Graph.CONNECTIVITY_8)) {
-            detections.add(c.centroid().roundToInteger());
+            detections.add(c.centroid());
             detections.lastElement().val = null;
         }
 
         return detections;
     }
 
+    // TODO: boundary pixels!! coordinates would go negative!! especially with the zero-based indexing
+    // TODO: refactor...!
+    public static Vector<Point<Double>> ExponentialGaussianEstimator(FloatProcessor fp, Vector<Point> detections) {
+        Vector<Point<Double>> fits = new Vector<>();
+        
+        for(int d = 0, dm = detections.size(); d < dm; d++)
+        {
+            Point p = detections.elementAt(d);
+            
+            // params = {x0,y0,Intensity,sigma,background}
+            double[] init_guess = new double[]{ p.getX().doubleValue(), p.getY().doubleValue(), fp.getPixelValue(p.roundToInteger().getX().intValue(), p.roundToInteger().getY().intValue()), 1.3, 100.0 };
+            double[][] x = new double[11 * 11][2];
+            double[] y = new double[11 * 11];
+            for (int r = 0; r < 11; r++) {
+                for (int c = 0; c < 11; c++) {
+                    int idx = r * 11 + c;
+                    x[idx][0] = (int) init_guess[0] + c - 5;  // x
+                    x[idx][1] = (int) init_guess[1] + r - 5;  // y
+                    y[idx] = new Float(fp.getPixelValue((int) x[idx][0], (int) x[idx][1])).doubleValue();    // G(x,y)
+                }
+            }
+            
+            LMA lma = new LMA(new Gaussian(), init_guess, y, x);
+            lma.fit();
+            
+            fits.add(new Point((lma.parameters[0]+0.5)*150.0, (lma.parameters[1]+0.5)*150.0));  // force pixelsize = 150nm + 0.5px shift to the center of each pixel
+        }
+        
+        return fits;
+    }
+
     public static void main(String[] args) throws UnexpectedException {
-        /**
-         * FITTING of 2D SYMMETRIC GAUSSIAN // // Generate the data double[]
-         * gen_params = new double[]{0.3, -0.8, 1.5, 1.8, 0.0}; double[][] x =
-         * new double[11 * 11][2]; double[] y = new double[11 * 11]; Gaussian
-         * gauss = new Gaussian(); for (int r = 0; r < 11; r++) { for (int c =
-         * 0; c < 11; c++) { int idx = r * 11 + c; x[idx][0] = c - 5; // x
-         * x[idx][1] = r - 5; // y y[idx] = gauss.getY(x[idx], gen_params); //
-         * G(x,y) } } // // Fit the parameters to recieve `gen_params` // params
-         * = {x0,y0,Intensity,sigma,background} double[] init_guess = new
-         * double[]{0.0, 0.0, 1.0, 1.0, 0.0}; LMA lma = new LMA(new Gaussian(),
-         * init_guess, y, x); lma.fit(); // // Print out the results
-         * System.out.println("iterations: " + lma.iterationCount);
-         * System.out.println( "chi2: " + lma.chi2 + ",\n" + "param0: " +
-         * lma.parameters[0] + ",\n" + "param1: " + lma.parameters[1] + ",\n" +
-         * "param2: " + lma.parameters[2] + ",\n" + "param3: " +
-         * lma.parameters[3] + ",\n" + "param4: " + lma.parameters[4]);
-         */
-        /**
-         * WAVELET DETECTOR
-         */
-        //ImagePlus image = IJ.openImage("../eye_00010.tif");
-        ImagePlus image = IJ.openImage("../tubulins1_00020.tif");
-        Vector<Point<Integer>> detections = WaveletDetector((FloatProcessor) image.getProcessor().convertToFloat(), false, true, false);
+        ImagePlus image = IJ.openImage("../eye_00010.tif");
+        //ImagePlus image = IJ.openImage("../tubulins1_00020.tif");
+        Vector<Point> detections = WaveletDetector((FloatProcessor) image.getProcessor().convertToFloat(), false, true, false);
+        System.out.println("DETECTION:");
         System.out.println(detections.toString());
+        System.out.println("");
+        System.out.println("LOCALIZATION:");
+        Vector<Point<Double>> fits = ExponentialGaussianEstimator((FloatProcessor) image.getProcessor().convertToFloat(), detections);
+        System.out.println(fits.toString());
         //FloatProcessor fp = WaveletDetector((FloatProcessor) image.getProcessor().convertToFloat(), false, true, false);
         //image.setProcessor(fp.convertToShort(false));
-        //IJ.save(image, "../rice_g1.tif");
-        /**/
+        //IJ.save(image, "../output.tif");
     }
 }
