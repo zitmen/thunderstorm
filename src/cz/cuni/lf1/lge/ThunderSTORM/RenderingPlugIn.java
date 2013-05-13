7f430d34967b0cb794d9e146c92c23bd10596fa9
Josef Borkovec <josef.borkovec@lf1.cuni.cz>
2013-05-13 12:15:04 +0200

    improved rendering plugin

--- src/cz/cuni/lf1/lge/ThunderSTORM/RenderingPlugIn.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/RenderingPlugIn.java
@@ -11,6 +11,11 @@ import ij.gui.GenericDialog;
 import ij.measure.ResultsTable;
 import ij.plugin.PlugIn;
 import ij.plugin.filter.Analyzer;
+import java.awt.Choice;
+import java.awt.TextField;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Vector;
 
 /**
  *
@@ -43,33 +48,66 @@ public class RenderingPlugIn implements PlugIn {
 
     GenericDialog gd = new GenericDialog("New Image");
     gd.addChoice("Method", METHODS, "ASH");
-    gd.addNumericField("Image_size_X", max(xpos), 2);
-    gd.addNumericField("Image_size_Y", max(ypos), 2);
+    gd.addNumericField("Image_size_X", Math.ceil(max(xpos)) + 2, 2);
+    gd.addNumericField("Image_size_Y", Math.ceil(max(ypos)) + 2, 2);
     gd.addNumericField("Resolution", 0.2, 3);
+    gd.addNumericField("dx (Localization accuracy)", 0.2, 3);
+    gd.addNumericField("ASH_shifts", 2, 0);
+    gd.addNumericField("Histogram_averages", 0, 0);
+
+    Vector choices = gd.getChoices();
+    final Choice ch = (Choice) choices.get(0);
+
+    Vector numericFields = gd.getNumericFields();
+    final TextField shiftsField = (TextField) numericFields.get(4);
+    final TextField avgField = (TextField) numericFields.get(5);
+
+    avgField.setEnabled(false);
+    ch.addItemListener(new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+        String selected = (String) e.getItem();
+        if ("ASH".equals(selected)) {
+          avgField.setEnabled(false);
+          shiftsField.setEnabled(true);
+        } else if ("Histogram".equals(selected)) {
+          avgField.setEnabled(true);
+          shiftsField.setEnabled(false);
+        } else {
+          avgField.setEnabled(false);
+          shiftsField.setEnabled(false);
+        }
+      }
+    });
+
+
 
     gd.showDialog();
     if (gd.wasCanceled()) {
       return;
     }
     String selectedMethod = gd.getNextChoice();
-    int imSizeX = (int) gd.getNextNumber();
-    int imSizeY = (int) gd.getNextNumber();
+    double imSizeX = gd.getNextNumber();
+    double imSizeY = gd.getNextNumber();
     double resolution = gd.getNextNumber();
+    double dx = gd.getNextNumber();
+    int shifts = (int) gd.getNextNumber();
+    int avg = (int) gd.getNextNumber();
 
     RenderingMethod renderer;
     if ("Density".equals(selectedMethod)) {
       renderer = new DensityRendering.Builder().resolution(resolution).roi(0, imSizeX, 0, imSizeY).build();
     } else if ("ASH".equals(selectedMethod)) {
-      renderer = new ASHRendering.Builder().resolution(resolution).roi(0, imSizeX, 0, imSizeY).shifts(2).build();
+      renderer = new ASHRendering.Builder().resolution(resolution).roi(0, imSizeX, 0, imSizeY).shifts(shifts).build();
     } else if ("Histogram".equals(selectedMethod)) {
-      renderer = new HistogramRendering.Builder().resolution(resolution).roi(0, imSizeX, 0, imSizeY).build();
+      renderer = new HistogramRendering.Builder().resolution(resolution).roi(0, imSizeX, 0, imSizeY).average(avg).build();
     } else if ("Scatter".equals(selectedMethod)) {
       renderer = new ScatterRendering.Builder().resolution(resolution).roi(0, imSizeX, 0, imSizeY).build();
     } else {
       IJ.error("Unknown rendering method. " + selectedMethod);
       return;
     }
-    new ImagePlus(renderer.getClass().getSimpleName(), renderer.getRenderedImage(xpos, ypos, 0.2)).show();
+    new ImagePlus(renderer.getClass().getSimpleName(), renderer.getRenderedImage(xpos, ypos, dx)).show();
 
   }
 
