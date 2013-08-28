713a3a8adcbe6f506a2bfd8cae423a0b9ef9ff54
Josef Borkovec <josef.borkovec@lf1.cuni.cz>
2013-08-28 09:48:15 +0200

    comments only

--- src/cz/cuni/lf1/lge/ThunderSTORM/rendering/DensityRendering.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/rendering/DensityRendering.java
@@ -44,6 +44,9 @@ public class DensityRendering extends AbstractRendering implements IncrementalRe
 
     @Override
     protected void drawPoint(double x, double y, double z, double dx) {
+        //3D gaussian blob integrated in over voxel 
+        //mathematica function for definite integral:
+        //Integrate[1/(Sqrt[(2*Pi)^3*s1^2*s2^2*s3^2]*E^((1/2)*((x - x0)^2/s1^2 + (y - y0)^2/s2^2 + (z - z0)^2/s3^2))), {z, a, b}, {x, ax, ax + 1}, {y, ay , ay + 1}]
         final int MAXRADIUS = (int) (defaultDX / resolution * 5);
         if(isInBounds(x, y)) {
             x = (x - xmin) / resolution;
@@ -75,12 +78,6 @@ public class DensityRendering extends AbstractRendering implements IncrementalRe
                             for(int idy = v - actualRadius; idy <= v + actualRadius; idy++) {
                                 if(idy >= 0 && idy < imSizeY) {
                                     double dify = idy - y;
-
-                                    //3D gaussian blob integrated in z 
-                                    //mathematica function for definite integral:
-                                    //Integrate[(1/Sqrt[(2 Pi)^3 *s1^2* s2^2* s3^2]) * E^(-1/2*((x - x0)^2/(s1^2) + (y - y0)^2/(s2^2) + (z - z0)^2/(s3^2))), {z, a, b}]
-
-
 
                                     double val = 0.125
                                             * xerfdif
