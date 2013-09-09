2ad47a251eb214151f5928dfa579dd549f8e69fa
Josef Borkovec <josef.borkovec@lf1.cuni.cz>
2013-09-09 10:59:06 +0200

    Centroid detetector now does watershed on grayscale image, upscaling removed.

--- src/cz/cuni/lf1/lge/ThunderSTORM/detectors/CentroidOfConnectedComponentsDetector.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/detectors/CentroidOfConnectedComponentsDetector.java
@@ -1,22 +1,22 @@
 package cz.cuni.lf1.lge.ThunderSTORM.detectors;
 
 import cz.cuni.lf1.lge.ThunderSTORM.detectors.ui.IDetectorUI;
-import static cz.cuni.lf1.lge.ThunderSTORM.util.ImageProcessor.applyMask;
-import static cz.cuni.lf1.lge.ThunderSTORM.util.ImageProcessor.threshold;
 import cz.cuni.lf1.lge.ThunderSTORM.FormulaParser.FormulaParserException;
 import cz.cuni.lf1.lge.ThunderSTORM.thresholding.Thresholder;
 import cz.cuni.lf1.lge.ThunderSTORM.util.Graph;
 import cz.cuni.lf1.lge.ThunderSTORM.util.GridBagHelper;
+import static cz.cuni.lf1.lge.ThunderSTORM.util.ImageProcessor.applyMask;
+import static cz.cuni.lf1.lge.ThunderSTORM.util.ImageProcessor.threshold;
 import cz.cuni.lf1.lge.ThunderSTORM.util.Point;
 import ij.Macro;
 import ij.Prefs;
+import ij.plugin.filter.MaximumFinder;
 import ij.plugin.frame.Recorder;
 import ij.process.ByteProcessor;
 import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 import java.awt.GridBagLayout;
 import java.util.Vector;
-import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -30,60 +30,48 @@ import javax.swing.JTextField;
 public final class CentroidOfConnectedComponentsDetector extends IDetectorUI implements IDetector {
 
     private final String name = "Centroid of connected components";
-    private boolean upsample;
     private String threshold;
     private transient float thresholdValue;
     private transient JTextField thrTextField;
-    private transient JCheckBox upCheckBox;
     private transient final static String DEFAULT_THRESHOLD = "std(I-Wave.V1)";
-    private transient final static boolean DEFAULT_UPSAMPLE = false;
 
     public CentroidOfConnectedComponentsDetector() throws FormulaParserException {
-        this(DEFAULT_UPSAMPLE, DEFAULT_THRESHOLD);
+        this(DEFAULT_THRESHOLD);
     }
 
     /**
      * Filter initialization.
      *
-     * @param upsample if {@code true}, the input image will be upsamplex by
-     * factor of 2 to achieve more accurate recognition of two or more molecules
-     * that are connected together
      * @param threshold a threshold value of intensity
      */
-    public CentroidOfConnectedComponentsDetector(boolean upsample, String threshold) throws FormulaParserException {
-        this.upsample = upsample;
+    public CentroidOfConnectedComponentsDetector(String threshold) throws FormulaParserException {
         this.threshold = threshold;
     }
 
     /**
-     * Detection algorithm works simply by applying a binary threshold on an
-     * input image, then by upsampling, if it is set to be done, and finally
-     * simple recognition if there is only one molecule or more molecules in a
-     * single group of pixels region.
+     * Detection algorithm works simply by setting all values lower than a
+     * threshold to zero, splitting close peaks by watershed and
+     * finding centroids of connected components.
      *
      * In more detail this is how it is done:
      * <ol>
-     * <li>apply the binary threshold</li>
-     * <li>if upsampling was enabled, upsample by factor of 2</li>
-     * <li>
-     * perform a watershed transform (this is the trick for recognition of more
-     * connected molecules; also this is why the upsampling can help, because
-     * the distance transform in the watershed algorithm will assign the same
-     * values to all the pixels if there are two connected molecules of radius
-     * 1; after the resampling, this boundary case is no more an issue); the
-     * result of the watershed transform is the same looking image but isteda of
-     * intensities pixels contain molecule id
+     * <li>apply the threshold to get thresholded binary image</li>
+     * <li>in the original image, set intensity to zero, where the thresholded
+     * image is zero. Leave the grayscale value otherwise.
      * </li>
      * <li>
-     * then we think of the image resulting of the watershed transform as an
+     * perform a watershed transform (this is the trick for recognition of more
+     * connected molecules
+     * </li>
+     * <li>AND the thresholded image with watershed image</li>
+     * <li>
+     * then we think of the resulting image as an
      * undirected graph with 8-connectivity and find all connected components
      * with the same id
      * </li>
      * <li>
      * finally, positions of molecules are calculated as centroids of components
-     * with the same id and ,of course, if the resampling was applied at the
-     * beginning of the algorithm, then the positions are downsampled to
-     * correspond with the original size of the input image
+     * with the same id 
      * </li>
      * </ol>
      *
@@ -93,27 +81,21 @@ public final class CentroidOfConnectedComponentsDetector extends IDetectorUI imp
      */
     @Override
     public Vector<Point> detectMoleculeCandidates(FloatProcessor image) throws FormulaParserException {
-        // thresholding first to make the image binary
+
         thresholdValue = Thresholder.getThreshold(threshold);
-        threshold(image, thresholdValue, 0.0f, 255.0f);
-        // watershed transform with[out] upscaling
-        if(upsample) {
-            image.setInterpolationMethod(FloatProcessor.NEAREST_NEIGHBOR);
-            image = (FloatProcessor) image.resize(image.getWidth() * 2);
-        }
-        // run the watershed algorithm - it works only with ByteProcessor! that's all I need though
-        ByteProcessor w = (ByteProcessor) image.convertToByte(false);
-        ImageJ_EDM edm = new ImageJ_EDM();  // use this version instead of built-in ImageJ version,
-        edm.setup("watershed", null);       // because the build-in version uses showStatus and
-        edm.run(w);                         // showProgress methods and it disturbs my progress/status info
-        image = applyMask((FloatProcessor) w.convertToFloat(), image);
+        FloatProcessor thresholdedImage = (FloatProcessor) image.duplicate();
+        threshold(thresholdedImage, thresholdValue, 0.0f, 255.0f);
+
+        FloatProcessor maskedImage = applyMask(image, thresholdedImage);
+
+        ImageJ_MaximumFinder watershedImpl = new ImageJ_MaximumFinder();
+        ByteProcessor watershedImage = watershedImpl.findMaxima(maskedImage, 0, ImageProcessor.NO_THRESHOLD, MaximumFinder.SEGMENTED, false, false);
+
+        FloatProcessor thresholdImageANDWatershedImage = applyMask(thresholdedImage, (FloatProcessor) watershedImage.convertToFloat());
         // finding a center of gravity (with subpixel precision)
         Vector<Point> detections = new Vector<Point>();
-        for(Graph.ConnectedComponent c : Graph.getConnectedComponents((ImageProcessor) image, Graph.CONNECTIVITY_8)) {
+        for(Graph.ConnectedComponent c : Graph.getConnectedComponents((ImageProcessor) thresholdImageANDWatershedImage, Graph.CONNECTIVITY_8)) {
             detections.add(c.centroid());
-            if(upsample) {
-                detections.lastElement().scaleXY(0.5);
-            }
             detections.lastElement().val = null;
         }
         return detections;
@@ -127,23 +109,18 @@ public final class CentroidOfConnectedComponentsDetector extends IDetectorUI imp
     @Override
     public JPanel getOptionsPanel() {
         thrTextField = new JTextField(Prefs.get("thunderstorm.detectors.centroid.thr", DEFAULT_THRESHOLD), 20);
-        upCheckBox = new JCheckBox("upsample");
-        upCheckBox.setSelected(Prefs.get("thunderstorm.detectors.centroid.upsample", DEFAULT_UPSAMPLE));
         //
         JPanel panel = new JPanel(new GridBagLayout());
         panel.add(new JLabel("Peak intensity threshold: "), GridBagHelper.leftCol());
         panel.add(thrTextField, GridBagHelper.rightCol());
-        panel.add(upCheckBox, GridBagHelper.pos_size(0, 1, 2, 1));
         return panel;
     }
 
     @Override
     public void readParameters() {
         threshold = thrTextField.getText();
-        upsample = upCheckBox.isSelected();
-        
+
         Prefs.set("thunderstorm.detectors.centroid.thr", threshold);
-        Prefs.set("thunderstorm.detectors.centroid.upsample", upsample);
     }
 
     @Override
@@ -156,20 +133,15 @@ public final class CentroidOfConnectedComponentsDetector extends IDetectorUI imp
         if(!DEFAULT_THRESHOLD.equals(threshold)) {
             Recorder.recordOption("threshold", threshold);
         }
-        if(DEFAULT_UPSAMPLE != upsample) {
-            Recorder.recordOption("upsample", Boolean.toString(upsample));
-        }
     }
 
     @Override
     public void readMacroOptions(String options) {
         threshold = Macro.getValue(options, "threshold", DEFAULT_THRESHOLD);
-        upsample = Boolean.parseBoolean(Macro.getValue(options, "upsample", Boolean.toString(upsample)));
     }
 
     @Override
     public void resetToDefaults() {
-        upCheckBox.setSelected(DEFAULT_UPSAMPLE);
         thrTextField.setText(DEFAULT_THRESHOLD);
     }
 
