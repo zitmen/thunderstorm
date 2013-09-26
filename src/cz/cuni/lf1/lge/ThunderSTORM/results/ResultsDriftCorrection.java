5c97305a2cba8d92b73aaccd3ad01d7842dc79b4
Josef Borkovec <josef.borkovec@lf1.cuni.cz>
2013-09-26 10:41:08 +0200

    drift correction now uses smoothing when interpolating the drift for all frames

--- src/cz/cuni/lf1/lge/ThunderSTORM/results/ResultsDriftCorrection.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/results/ResultsDriftCorrection.java
@@ -30,7 +30,6 @@ import javax.swing.SwingConstants;
 import javax.swing.SwingWorker;
 import static cz.cuni.lf1.lge.ThunderSTORM.util.Math.min;
 import static cz.cuni.lf1.lge.ThunderSTORM.util.Math.max;
-import ij.IJ;
 
 public class ResultsDriftCorrection {
 
@@ -151,7 +150,7 @@ public class ResultsDriftCorrection {
             driftY[i] = offset.y;
         }
         Plot plot = new Plot("drift", "frame", "drift [px]", grid, driftX);
-        plot.setLimits(minFrame, driftCorrection.getMaxFrame(), Math.min(min(driftX),min(driftY)), Math.max(max(driftX),max(driftY)));
+        plot.setLimits(minFrame, driftCorrection.getMaxFrame(), Math.min(min(driftCorrection.getBinDriftX()),min(driftCorrection.getBinDriftY())), Math.max(max(driftX),max(driftY)));
         plot.setColor(Color.blue);
         plot.addPoints(driftCorrection.getBinCenters(), driftCorrection.getBinDriftX(), Plot.CROSS);
         plot.addLabel(0.05, 0.8, "x drift");
