ca4b98ef3009cba43fdbba48c6cc5d690ca921d1
Josef Borkovec <josef.borkovec@lf1.cuni.cz>
2013-11-22 10:10:12 +0100

    About ThunderSTORM shows html page. No content yet.

--- src/cz/cuni/lf1/lge/ThunderSTORM/AboutPlugIn.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/AboutPlugIn.java
@@ -1,32 +1,71 @@
 package cz.cuni.lf1.lge.ThunderSTORM;
 
+import cz.cuni.lf1.lge.ThunderSTORM.UI.Help;
 import ij.IJ;
-import ij.ImagePlus;
+import ij.plugin.BrowserLauncher;
 import ij.plugin.PlugIn;
-import ij.process.ImageProcessor;
-import java.awt.Image;
-import java.awt.image.ImageProducer;
-import java.net.URL;
+import java.awt.Cursor;
+import java.awt.Dialog;
+import java.awt.Dimension;
+import java.awt.Window;
+import javax.swing.BorderFactory;
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import org.fit.cssbox.swingbox.BrowserPane;
+import org.fit.cssbox.swingbox.SwingBoxEditorKit;
 
 public class AboutPlugIn implements PlugIn {
 
     public static final String FILE_NAME = "about.png";
-    
+    private static final String url = "resources/help/help.html";
+    private static final int WINDOW_WIDTH = 600;
+    private static final int WINDOW_HEIGHT = 600;
+
     @Override
     public void run(String arg) {
-        ImageProcessor ip = null;
-        URL url = getClass().getResource("/" + FILE_NAME);
-        if (url != null) {
-            Image img = null;
-            try {
-                img = IJ.getInstance().createImage((ImageProducer)url.getContent());
-            } catch (Exception e) {
-            }
-            if (img != null) {
-                ImagePlus imp = new ImagePlus("About ThunderSTORM", img);
-                ip = imp.getProcessor();
-                imp.show();
-            }
+        JDialog dialog = new JDialog(IJ.getInstance(), "About ThunderSTORM");
+        if(IJ.isJava17()) {
+            dialog.setType(Window.Type.UTILITY);
         }
+        dialog.setModalExclusionType(Dialog.ModalExclusionType.APPLICATION_EXCLUDE); //for use within modal dialog
+        final BrowserPane htmlBrowser = new BrowserPane();
+        htmlBrowser.setEditorKit(new SwingBoxEditorKit());
+        htmlBrowser.setBorder(BorderFactory.createEmptyBorder());
+        htmlBrowser.addHyperlinkListener(new HyperlinkListener() {
+            @Override
+            public void hyperlinkUpdate(HyperlinkEvent e) {
+                if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                    try {
+                        if("jar".equals(e.getURL().getProtocol())) {
+                            htmlBrowser.setPage(e.getURL());
+                        } else {
+                            BrowserLauncher.openURL(e.getURL().toString());
+                        }
+                    } catch(Exception ex) {
+                        IJ.handleException(ex);
+                    }
+                } else if(e.getEventType() == HyperlinkEvent.EventType.ENTERED) {
+                    htmlBrowser.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                } else if(e.getEventType() == HyperlinkEvent.EventType.EXITED) {
+                    htmlBrowser.setCursor(Cursor.getDefaultCursor());
+                }
+
+            }
+        });
+        JScrollPane scrollPane = new JScrollPane(htmlBrowser);
+        scrollPane.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        dialog.getContentPane().add(scrollPane);
+
+        try {
+            htmlBrowser.setPage(Help.getUrl(url));
+        } catch(Exception e) {
+            htmlBrowser.setText("Could not load resource file.");
+        }
+
+        dialog.pack();
+        dialog.setLocationRelativeTo(null);
+        dialog.setVisible(true);
     }
 }
