1fdab302e2e430ae3570ba37c275be75ce04e705
Josef Borkovec <josef.borkovec@lf1.cuni.cz>
2014-01-02 12:39:06 +0100

    version 1.0 that was submitted to Nature Methods

--- src/cz/cuni/lf1/lge/ThunderSTORM/ThunderSTORM.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/ThunderSTORM.java
@@ -2,7 +2,7 @@ package cz.cuni.lf1.lge.ThunderSTORM;
 
 public class ThunderSTORM {
     
-    public static final String VERSION = "dev-2013-12-03-b2";
+    public static final String VERSION = "1.0-2013-12-03";
     public static final String URL_DAILY = "https://googledrive.com/host/0BzOGc-xMFyDYR1JaelZYQmJsaUE/builds/daily";
     public static final String URL_STABLE = "https://googledrive.com/host/0BzOGc-xMFyDYR1JaelZYQmJsaUE/builds/stable";
     public static final String FILE_NAME = "Thunder_STORM";
