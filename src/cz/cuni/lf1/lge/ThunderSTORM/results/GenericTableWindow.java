0aae036585ad5dcafc016f1b9593b5d8b4d32527
Josef Borkovec <josef.borkovec@lf1.cuni.cz>
2014-03-28 14:30:34 +0100

    Added macro commands for changing units in results table

--- src/cz/cuni/lf1/lge/ThunderSTORM/results/GenericTableWindow.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/results/GenericTableWindow.java
@@ -44,12 +44,12 @@ import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
 public class GenericTableWindow {
-
+    
     protected JFrame frame;
     protected ColoredTable table;
     protected final TripleStateTableModel model;
     protected JScrollPane tableScrollPane;
-
+    
     public GenericTableWindow(String frameTitle) {
         frame = new JFrame(frameTitle);
         frame.setIconImage(IJ.getInstance().getIconImage());
@@ -74,7 +74,7 @@ public class GenericTableWindow {
             public void mouseDragged(MouseEvent e) {
                 tableMouseDragged(e);
             }
-
+            
             @Override
             public void mouseMoved(MouseEvent e) {
                 tableMouseMoved(e);
@@ -85,49 +85,49 @@ public class GenericTableWindow {
             public void mouseClicked(MouseEvent e) {
                 tableMouseClicked(e);
             }
-
+            
             @Override
             public void mousePressed(MouseEvent e) {
                 tableMousePressed(e);
             }
-
+            
             @Override
             public void mouseReleased(MouseEvent e) {
                 tableMouseReleased(e);
             }
-
+            
             @Override
             public void mouseEntered(MouseEvent e) {
                 tableMouseEntered(e);
             }
-
+            
             @Override
             public void mouseExited(MouseEvent e) {
                 tableMouseExited(e);
             }
         });
     }
-
+    
     protected void packFrame() {
         Container contentPane = frame.getContentPane();
         contentPane.add(tableScrollPane);
         frame.setContentPane(contentPane);
         frame.pack();
     }
-
+    
     public TripleStateTableModel getModel() {
         return (TripleStateTableModel) table.getModel();
     }
-
+    
     public JTable getView() {
         return table;
     }
-
+    
     public void show(String title) {
         frame.setTitle(title);
         show();
     }
-
+    
     public void show() {
         WindowManager.addWindow(frame); // ImageJ's own Window Manager
         SwingUtilities.invokeLater(new Runnable() {
@@ -138,20 +138,20 @@ public class GenericTableWindow {
         });
         WindowManager.setWindow(frame); // ImageJ's own Window Manager
     }
-
+    
     public void hide() {
         frame.setVisible(false);
         WindowManager.removeWindow(frame); // ImageJ's own Window Manager
     }
-
+    
     public boolean isVisible() {
         return frame.isVisible();
     }
-
+    
     private class UnitsContextMenu implements ActionListener {
-
+        
         private int column;
-
+        
         public UnitsContextMenu(MouseEvent e, int column) {
             this.column = column;
             Units selected = model.getColumnUnits(column);
@@ -164,6 +164,9 @@ public class GenericTableWindow {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         model.convertAllColumnsToDigitalUnits();
+                        if(GenericTableWindow.this instanceof ResultsTableWindow) {
+                            TableHandlerPlugin.recordChangeAllUnits(false);
+                        }
                     }
                 });
                 popup.add(item);
@@ -172,6 +175,9 @@ public class GenericTableWindow {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         model.convertAllColumnsToAnalogUnits();
+                        if(GenericTableWindow.this instanceof ResultsTableWindow) {
+                            TableHandlerPlugin.recordChangeAllUnits(true);
+                        }
                     }
                 });
                 popup.add(item);
@@ -192,8 +198,8 @@ public class GenericTableWindow {
                 if(unit == Units.UNITLESS) {
                     continue;
                 }
-                if((PSFModel.Params.LABEL_Z.equals(model.getColumnRealName(column))) ||
-                        (PSFModel.Params.LABEL_Z_REL.equals(model.getColumnRealName(column)))) {
+                if((PSFModel.Params.LABEL_Z.equals(model.getColumnRealName(column)))
+                        || (PSFModel.Params.LABEL_Z_REL.equals(model.getColumnRealName(column)))) {
                     if(unit == Units.PIXEL) {
                         continue;   // z-position can't be converted to pixels
                     }
@@ -204,7 +210,7 @@ public class GenericTableWindow {
             }
             popup.show(e.getComponent(), e.getX(), e.getY());
         }
-
+        
         @Override
         public void actionPerformed(ActionEvent e) {
             Units target = Units.fromString(e.getActionCommand());
@@ -219,11 +225,14 @@ public class GenericTableWindow {
             } else {
                 model.setColumnUnits(column, target);
             }
+            if(GenericTableWindow.this instanceof ResultsTableWindow) {
+                TableHandlerPlugin.recordChangeColumnUnits(colName, target);
+            }
         }
     }
-
+    
     private class TableDropTarget extends DropTarget {
-
+        
         @Override
         public synchronized void drop(DropTargetDropEvent dtde) {
             dtde.acceptDrop(DnDConstants.ACTION_REFERENCE);
@@ -240,48 +249,48 @@ public class GenericTableWindow {
             dropFile(f);
         }
     }
-
+    
     protected void dropFile(File f) {
         new ImportExportPlugIn(f.getAbsolutePath()).run(ImportExportPlugIn.IMPORT + IJResultsTable.IDENTIFIER);
     }
-
+    
     protected void tableHeaderMouseClicked(MouseEvent e) {
         if(SwingUtilities.isRightMouseButton(e)) {
             new UnitsContextMenu(e, table.convertColumnIndexToModel(table.columnAtPoint(e.getPoint())));
         }
     }
-
+    
     protected void tableMouseDragged(MouseEvent e) {
         //
     }
-
+    
     protected void tableMouseMoved(MouseEvent e) {
         //
     }
-
+    
     protected void tableMouseClicked(MouseEvent e) {
         //
     }
-
+    
     protected void tableMousePressed(MouseEvent e) {
         //
     }
-
+    
     protected void tableMouseReleased(MouseEvent e) {
         //
     }
-
+    
     protected void tableMouseEntered(MouseEvent e) {
         //
     }
-
+    
     protected void tableMouseExited(MouseEvent e) {
         //
     }
 
     // =============================================================
     public class ColoredTable extends JTable {
-
+        
         public final Color LIGHT_ORANGE = new Color(255, 222, 200);
         public final Color LIGHT_RED = new Color(255, 222, 222);
         public final Color LIGHT_GREEN = new Color(222, 255, 222);
@@ -294,24 +303,24 @@ public class GenericTableWindow {
         public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
             final Component c = super.prepareRenderer(renderer, row, column);
             NumberFormat formatter = NumberFormat.getInstance(Locale.ENGLISH);
-            ((DecimalFormat)formatter).setGroupingUsed(false);
-            ((JLabel)c).setText(formatter.format(getValueAt(row, column)));
+            ((DecimalFormat) formatter).setGroupingUsed(false);
+            ((JLabel) c).setText(formatter.format(getValueAt(row, column)));
             if(!isCellSelected(row, column)) {
                 c.setBackground(null);
-                Molecule mol = ((GenericTableModel)super.getModel()).getRow(convertRowIndexToModel(row));
+                Molecule mol = ((GenericTableModel) super.getModel()).getRow(convertRowIndexToModel(row));
                 switch(mol.getStatus()) {
                     case FALSE_POSITIVE:
                         c.setBackground(LIGHT_RED);
                         break;
-
+                    
                     case FALSE_NEGATIVE:
                         c.setBackground(LIGHT_ORANGE);
                         break;
-                        
+                    
                     case TRUE_POSITIVE:
                         c.setBackground(LIGHT_GREEN);
                         break;
-
+                    
                     default:
                         c.setBackground(Color.WHITE);
                 }
--- src/cz/cuni/lf1/lge/ThunderSTORM/results/TableHandlerPlugin.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/results/TableHandlerPlugin.java
@@ -1,6 +1,9 @@
 package cz.cuni.lf1.lge.ThunderSTORM.results;
 
 import cz.cuni.lf1.lge.ThunderSTORM.UI.GUI;
+import cz.cuni.lf1.lge.ThunderSTORM.estimators.PSF.MoleculeDescriptor;
+import cz.cuni.lf1.lge.ThunderSTORM.estimators.PSF.MoleculeDescriptor.Units;
+import cz.cuni.lf1.lge.ThunderSTORM.estimators.PSF.PSFModel;
 import ij.IJ;
 import ij.Macro;
 import ij.plugin.PlugIn;
@@ -9,25 +12,57 @@ import java.util.List;
 
 public class TableHandlerPlugin implements PlugIn {
 
+    private static final String TARGET_UNITS_KEY = "targetUnits";
+    private static final String ACTION_KEY = "action";
+    private static final String COLUMN_KEY = "column";
+
+    private static final String SHOW = "show";
+    private static final String RESET = "reset";
+    private static final String UNDO_REDO = "undo-redo";
+    private static final String CHANGE_COLUMN_UNITS = "changeColumnUnits";
+    private static final String CHANGE_ALL_UNITS = "changeAllUnits";
+    private static final String SHOW_TABLE_COMMAND = "Show results table";
+    private static final String DIGITAL = "digital";
+    private static final String ANALOG = "analog";
+
     @Override
     public void run(String arg) {
         GUI.setLookAndFeel();
         //
         String options = Macro.getOptions();
-        String action = options == null ? "show" : Macro.getValue(options, "action", "show");
+        String action = options == null ? SHOW : Macro.getValue(options, ACTION_KEY, SHOW);
         try {
             IJResultsTable resultsTable = IJResultsTable.getResultsTable();
-            if("show".equals(action)) {
+            if(SHOW.equals(action)) {
                 resultsTable.show();
-            } else if("reset".equals(action)) {
+            } else if(RESET.equals(action)) {
                 resultsTable.copyOriginalToActual();
                 resultsTable.convertAllColumnsToAnalogUnits();
                 resultsTable.getOperationHistoryPanel().removeAllOperations();
                 resultsTable.setStatus("Results reset.");
                 resultsTable.showPreview();
-            } else if("undo-redo".equals(action)) {
+            } else if(UNDO_REDO.equals(action)) {
                 resultsTable.getOperationHistoryPanel().undoOrRedoLastOperation();
-
+            } else if(CHANGE_COLUMN_UNITS.equals(action)) {
+                String column = Macro.getValue(options, COLUMN_KEY, "");
+                if(!resultsTable.columnExists(column)) {
+                    throw new RuntimeException("Column \"" + column + "\" does not exist.");
+                }
+                MoleculeDescriptor.Units targetUnits = Units.fromString(Macro.getValue(options, TARGET_UNITS_KEY, ""));
+                if(PSFModel.Params.LABEL_X.equals(column) || PSFModel.Params.LABEL_Y.equals(column)) {
+                    // ensure that X and Y are always in same units!
+                    resultsTable.model.setColumnUnits(PSFModel.Params.LABEL_X, targetUnits);
+                    resultsTable.model.setColumnUnits(PSFModel.Params.LABEL_Y, targetUnits);
+                } else {
+                    resultsTable.model.setColumnUnits(column, targetUnits);
+                }
+            } else if(CHANGE_ALL_UNITS.equals(action)) {
+                String analogOrDigital = Macro.getValue(options, TARGET_UNITS_KEY, ANALOG);
+                if(analogOrDigital.equals(ANALOG)) {
+                    resultsTable.convertAllColumnsToAnalogUnits();
+                } else if(analogOrDigital.equals(DIGITAL)) {
+                    resultsTable.convertAllColumnsToDigitalUnits();
+                }
             } else {
                 List<? extends PostProcessingModule> modules = resultsTable.getPostProcessingModules();
 
@@ -52,16 +87,35 @@ public class TableHandlerPlugin implements PlugIn {
 
     public static void recordReset() {
         if(Recorder.record) {
-            Recorder.setCommand("Show results table");
-            Recorder.recordOption("action", "reset");
+            Recorder.setCommand(SHOW_TABLE_COMMAND);
+            Recorder.recordOption(ACTION_KEY, RESET);
             Recorder.saveCommand();
         }
     }
 
     public static void recordUndoOrRedo() {
         if(Recorder.record) {
-            Recorder.setCommand("Show results table");
-            Recorder.recordOption("action", "undo-redo");
+            Recorder.setCommand(SHOW_TABLE_COMMAND);
+            Recorder.recordOption(ACTION_KEY, UNDO_REDO);
+            Recorder.saveCommand();
+        }
+    }
+
+    public static void recordChangeColumnUnits(String column, MoleculeDescriptor.Units targetUnits) {
+        if(Recorder.record) {
+            Recorder.setCommand(SHOW_TABLE_COMMAND);
+            Recorder.recordOption(ACTION_KEY, CHANGE_COLUMN_UNITS);
+            Recorder.recordOption(COLUMN_KEY, column);
+            Recorder.recordOption(TARGET_UNITS_KEY, targetUnits.toString());
+            Recorder.saveCommand();
+        }
+    }
+
+    public static void recordChangeAllUnits(boolean analog) {
+        if(Recorder.record) {
+            Recorder.setCommand(SHOW_TABLE_COMMAND);
+            Recorder.recordOption(ACTION_KEY, CHANGE_ALL_UNITS);
+            Recorder.recordOption(TARGET_UNITS_KEY, analog ? ANALOG : DIGITAL);
             Recorder.saveCommand();
         }
     }
