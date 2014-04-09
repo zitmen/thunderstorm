2789b10473d19bfe5e4ae8e744c8716b3d1dfff5
Josef Borkovec <josef.borkovec@lf1.cuni.cz>
2014-04-09 18:14:20 +0200

    v1.2

--- src/cz/cuni/lf1/lge/ThunderSTORM/ThunderSTORM.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/ThunderSTORM.java
@@ -2,7 +2,7 @@ package cz.cuni.lf1.lge.ThunderSTORM;
 
 public class ThunderSTORM {
     
-    public static final String VERSION = "dev-2014-04-09-b2";
+    public static final String VERSION = "1.2-2014-04-09";
     public static final String URL_DAILY = "https://googledrive.com/host/0BzOGc-xMFyDYR1JaelZYQmJsaUE/builds/daily";
     public static final String URL_STABLE = "https://googledrive.com/host/0BzOGc-xMFyDYR1JaelZYQmJsaUE/builds/stable";
     public static final String FILE_NAME = "Thunder_STORM";
