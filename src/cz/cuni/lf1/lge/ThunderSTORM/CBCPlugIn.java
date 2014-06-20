e36b6fc1ba73c166c6aa23199741cd9e1d73f735
Martin Ovesny <zitmen@gmail.com>
2014-06-20 16:15:56 +0200

    Enhanced version of CBC plugin which now returns more information about the distribution of points accross the channels.

--- src/cz/cuni/lf1/lge/ThunderSTORM/CBCPlugIn.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/CBCPlugIn.java
@@ -16,16 +16,16 @@ import cz.cuni.lf1.lge.ThunderSTORM.util.VectorMath;
 import cz.cuni.lf1.lge.thunderstorm.util.macroui.ParameterKey;
 import cz.cuni.lf1.lge.thunderstorm.util.macroui.ParameterTracker;
 import cz.cuni.lf1.lge.thunderstorm.util.macroui.validators.DoubleValidatorFactory;
+import fiji.util.gui.GenericDialogPlus;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.Macro;
 import ij.plugin.PlugIn;
-import java.util.Vector;
 
 public class CBCPlugIn implements PlugIn {
 
     ParameterTracker params = new ParameterTracker();
-    ParameterKey.Double maxDist = params.createDoubleField("maxDist", DoubleValidatorFactory.positiveNonZero(), 10);
+    //ParameterKey.Double maxDist = params.createDoubleField("maxDist", DoubleValidatorFactory.positiveNonZero(), 10);
 
     public CBCPlugIn() {
         params.setNoGuiParametersAllowed(true);
@@ -39,7 +39,7 @@ public class CBCPlugIn implements PlugIn {
             } else {
                 params.recordMacroOptions();
             }
-
+            
             IJResultsTable resultsTable = IJResultsTable.getResultsTable();
             IJGroundTruthTable groundTruthTable = IJGroundTruthTable.getGroundTruthTable();
 
@@ -55,66 +55,98 @@ public class CBCPlugIn implements PlugIn {
                 IJ.error("Ground-truth table empty.");
                 return;
             }
-
-            double[][] firstXY = new double[resultsTable.getRowCount()][];
-            for(int i = 0; i < firstXY.length; i++) {
-                Molecule m = resultsTable.getRow(i);
-                firstXY[i] = new double[]{m.getX(MoleculeDescriptor.Units.PIXEL), m.getY(MoleculeDescriptor.Units.PIXEL)};
+            
+            GenericDialogPlus gd = new GenericDialogPlus("ThunderSTORM: Performance evaluation");
+            gd.addNumericField("Radius step [nm]: ", 100, 0);
+            gd.addNumericField("Step count: ", 5, 0);
+            gd.addCheckbox("Add CBC into the results table: ", true);
+            gd.addCheckbox("Add distance to the nearest neighbor into the results table: ", true);
+            gd.addCheckbox("Add count of neighbors in within a radius into the results table: ", true);
+            gd.showDialog();
+            if(!gd.wasCanceled()) {
+                double radiusStep = gd.getNextNumber();
+                int radiusCount = (int)gd.getNextNumber();
+                boolean addCBCToTable = gd.getNextBoolean();
+                boolean addNNDistToTable = gd.getNextBoolean();
+                boolean addNeighborsInDistToTable = gd.getNextBoolean();
+                runCBC(radiusStep, radiusCount, addCBCToTable, addNNDistToTable, addNeighborsInDistToTable);
             }
-            double[][] secondXY = new double[groundTruthTable.getRowCount()][];
-            for(int i = 0; i < secondXY.length; i++) {
-                Molecule m = groundTruthTable.getRow(i);
-                secondXY[i] = new double[]{m.getX(MoleculeDescriptor.Units.PIXEL), m.getY(MoleculeDescriptor.Units.PIXEL)};
-            }
-
-            int radiusCount = 200;
-            double radiusStep = maxDist.getValue() / radiusCount;
-            CBC cbc = new CBC(firstXY, secondXY, radiusStep, radiusCount);
-
-            IJ.showStatus("Calculating first channel CBC.");
-            double[] firstChannelCBC = cbc.calculateFirstChannelCBC();
-            addColumnToModel(resultsTable.getModel(), firstChannelCBC, "cbc", Units.UNITLESS);
-            double[] x1 = resultsTable.getColumnAsDoubles(PSFModel.Params.LABEL_X, MoleculeDescriptor.Units.PIXEL);
-            double[] y1 = resultsTable.getColumnAsDoubles(PSFModel.Params.LABEL_Y, MoleculeDescriptor.Units.PIXEL);
-
-            double[] x2 = groundTruthTable.getColumnAsDoubles(PSFModel.Params.LABEL_X, MoleculeDescriptor.Units.PIXEL);
-            double[] y2 = groundTruthTable.getColumnAsDoubles(PSFModel.Params.LABEL_Y, MoleculeDescriptor.Units.PIXEL);
-            double maxRoiX = MathProxy.max(VectorMath.max(x1), VectorMath.max(x2));
-            double maxRoiY = MathProxy.max(VectorMath.max(y1), VectorMath.max(y2));
-
-            RenderingMethod renderer = new ASHRendering.Builder()
-                    .shifts(2)
-                    .zShifts(1).colorizeZ(true).zRange(-1, 1, 0.1)
-                    .roi(0, maxRoiX, 0, maxRoiY)
-                    .resolution(0.2)
-                    .build();
-            ImagePlus imp = renderer.getRenderedImage(x1, y1, firstChannelCBC, null);
-            imp.show();
-
-            IJ.showStatus("Calculating second channel CBC.");
-            double[] secondChannelCBC = cbc.calculateSecondChannelCBC();
-            RenderingMethod renderer2 = new ASHRendering.Builder()
-                    .shifts(2)
-                    .zShifts(1).colorizeZ(true).zRange(-1, 1, 0.1)
-                    .roi(0, maxRoiX, 0, maxRoiY)
-                    .resolution(0.2)
-                    .build();
-
-            ImagePlus imp2 = renderer2.getRenderedImage(x2, y2, secondChannelCBC, null);
-            imp2.show();
-            IJ.showStatus("Done.");
         } catch(Exception e) {
             IJ.handleException(e);
         }
     }
+    
+    public void runCBC(double radiusStep, int radiusCount, boolean addCBCToTable, boolean addNNDistToTable, boolean addNeighborsInDistToTable) {
+        IJResultsTable resultsTable = IJResultsTable.getResultsTable();
+        IJGroundTruthTable groundTruthTable = IJGroundTruthTable.getGroundTruthTable();
 
-    private void addColumnToModel(GenericTableModel model, final double[] firstChannelCBC, String cbc, Units units) {
-            model.addColumn(cbc, units, new IValue<Double>() {
+        double[][] firstXY = new double[resultsTable.getRowCount()][];
+        for(int i = 0; i < firstXY.length; i++) {
+            Molecule m = resultsTable.getRow(i);
+            firstXY[i] = new double[]{m.getX(MoleculeDescriptor.Units.PIXEL), m.getY(MoleculeDescriptor.Units.PIXEL)};
+        }
+        double[][] secondXY = new double[groundTruthTable.getRowCount()][];
+        for(int i = 0; i < secondXY.length; i++) {
+            Molecule m = groundTruthTable.getRow(i);
+            secondXY[i] = new double[]{m.getX(MoleculeDescriptor.Units.PIXEL), m.getY(MoleculeDescriptor.Units.PIXEL)};
+        }
+
+        CBC cbc = new CBC(firstXY, secondXY, radiusStep, radiusCount);
+
+        IJ.showStatus("Calculating first channel CBC.");
+        double[] firstChannelCBC = cbc.calculateFirstChannelCBC();
+        if(addCBCToTable) addColumnToModel(resultsTable.getModel(), firstChannelCBC, "cbc", Units.UNITLESS);
+        if(addNNDistToTable) addColumnToModel(resultsTable.getModel(), cbc.firstChannelNearestNeighborDistances, "nn_dist", Units.PIXEL);
+        if(addNeighborsInDistToTable) {
+            for(int i = 0; i < cbc.squaredRadiusDomainNm.length; i++) {
+                addColumnToModel(resultsTable.getModel(), cbc.firstChannelNeighborsInDistance[i], "neighbors_in_dist_"+((i+1)*radiusStep), Units.PIXEL);
+            }
+        }
+
+        double[] x1 = resultsTable.getColumnAsDoubles(PSFModel.Params.LABEL_X, MoleculeDescriptor.Units.PIXEL);
+        double[] y1 = resultsTable.getColumnAsDoubles(PSFModel.Params.LABEL_Y, MoleculeDescriptor.Units.PIXEL);
+        double[] x2 = groundTruthTable.getColumnAsDoubles(PSFModel.Params.LABEL_X, MoleculeDescriptor.Units.PIXEL);
+        double[] y2 = groundTruthTable.getColumnAsDoubles(PSFModel.Params.LABEL_Y, MoleculeDescriptor.Units.PIXEL);
+        double maxRoiX = MathProxy.max(VectorMath.max(x1), VectorMath.max(x2));
+        double maxRoiY = MathProxy.max(VectorMath.max(y1), VectorMath.max(y2));
+
+        RenderingMethod renderer = new ASHRendering.Builder()
+                .shifts(2)
+                .zShifts(1).colorizeZ(true).zRange(-1, 1, 0.1)
+                .roi(0, maxRoiX, 0, maxRoiY)
+                .resolution(0.2)
+                .build();
+        ImagePlus imp = renderer.getRenderedImage(x1, y1, firstChannelCBC, null);
+        imp.show();
+
+        IJ.showStatus("Calculating second channel CBC.");
+        double[] secondChannelCBC = cbc.calculateSecondChannelCBC();
+        if(addCBCToTable) addColumnToModel(groundTruthTable.getModel(), secondChannelCBC, "cbc", Units.UNITLESS);
+        if(addNNDistToTable) addColumnToModel(groundTruthTable.getModel(), cbc.secondChannelNearestNeighborDistances, "nn_dist", Units.PIXEL);
+        if(addNeighborsInDistToTable) {
+            for(int i = 0; i < cbc.squaredRadiusDomainNm.length; i++) {
+                addColumnToModel(groundTruthTable.getModel(), cbc.secondChannelNeighborsInDistance[i], "neighbors_in_dist_"+((i+1)*radiusStep), Units.PIXEL);
+            }
+        }
+
+        RenderingMethod renderer2 = new ASHRendering.Builder()
+                .shifts(2)
+                .zShifts(1).colorizeZ(true).zRange(-1, 1, 0.1)
+                .roi(0, maxRoiX, 0, maxRoiY)
+                .resolution(0.2)
+                .build();
+
+        ImagePlus imp2 = renderer2.getRenderedImage(x2, y2, secondChannelCBC, null);
+        imp2.show();
+        IJ.showStatus("Done.");
+    }
+
+    private void addColumnToModel(GenericTableModel model, final double[] data, String title, Units units) {
+            model.addColumn(title, units, new IValue<Double>() {
                 int i = 0;
-
                 @Override
                 public Double getValue() {
-                    return firstChannelCBC[i++];
+                    return data[i++];
                 }
             });
         model.fireTableStructureChanged();
--- src/cz/cuni/lf1/lge/ThunderSTORM/colocalization/CBC.java
+++ src/cz/cuni/lf1/lge/ThunderSTORM/colocalization/CBC.java
@@ -1,5 +1,6 @@
 package cz.cuni.lf1.lge.ThunderSTORM.colocalization;
 
+import cz.cuni.lf1.lge.ThunderSTORM.estimators.PSF.MoleculeDescriptor.Units;
 import cz.cuni.lf1.lge.ThunderSTORM.util.Loop;
 import cz.cuni.lf1.lge.ThunderSTORM.util.MathProxy;
 import cz.cuni.lf1.lge.ThunderSTORM.util.VectorMath;
@@ -25,16 +26,21 @@ public class CBC {
 
     private double[][] firstChannelXYdata;
     private double[][] secondChannelXYdata;
+    public double[] firstChannelNearestNeighborDistances;
+    public double[] secondChannelNearestNeighborDistances;
+    public double[][] firstChannelNeighborsInDistance;
+    public double[][] secondChannelNeighborsInDistance;
     private KDTree<double[]> firstChannelTree;
     private KDTree<double[]> secondChannelTree;
-    private double[] squaredRadiusDomain;
-    private double radiusStep;
+    public double[] squaredRadiusDomainPx;
+    public double[] squaredRadiusDomainNm;
+    private double radiusStepPx;
     private int radiusCount;
 
     public CBC(double[][] firstChannelXYdata, double[][] secondChannelXYdata, double radiusStep, int radiusCount) {
         this.firstChannelXYdata = firstChannelXYdata;
         this.secondChannelXYdata = secondChannelXYdata;
-        this.radiusStep = radiusStep;
+        this.radiusStepPx = Units.NANOMETER.convertTo(Units.PIXEL, radiusStep);
         this.radiusCount = radiusCount;
 
         fillRadiusDomain();
@@ -42,38 +48,44 @@ public class CBC {
     }
 
     public double[] calculateFirstChannelCBC() {
-        return calc(firstChannelXYdata, firstChannelTree, secondChannelTree);
+        firstChannelNeighborsInDistance = new double[squaredRadiusDomainPx.length][firstChannelXYdata.length];
+        firstChannelNearestNeighborDistances = new double[firstChannelXYdata.length];
+        return calc(firstChannelXYdata, firstChannelTree, secondChannelTree, firstChannelNeighborsInDistance, firstChannelNearestNeighborDistances);
     }
 
     public double[] calculateSecondChannelCBC() {
-        return calc(secondChannelXYdata, secondChannelTree, firstChannelTree);
+        secondChannelNeighborsInDistance = new double[squaredRadiusDomainPx.length][secondChannelXYdata.length];
+        secondChannelNearestNeighborDistances = new double[secondChannelXYdata.length];
+        return calc(secondChannelXYdata, secondChannelTree, firstChannelTree, secondChannelNeighborsInDistance, secondChannelNearestNeighborDistances);
     }
 
-    private double[] calc(final double[][] mainChannelXYdata, final KDTree<double[]> mainChannelTree, final KDTree<double[]> otherChannelTree) {
+    private double[] calc(final double[][] mainChannelXYdata, final KDTree<double[]> mainChannelTree, final KDTree<double[]> otherChannelTree, final double [][] neighborsInDistance, final double [] nearestNeighborDistances) {
 
-        final int lastRadiusIndex = squaredRadiusDomain.length - 1;
-        final double maxSquaredRadius = squaredRadiusDomain[lastRadiusIndex];
+        final int lastRadiusIndex = squaredRadiusDomainPx.length - 1;
+        final double maxSquaredRadius = squaredRadiusDomainPx[lastRadiusIndex];
 
         final double[] cbcResults = new double[mainChannelXYdata.length];
         final AtomicInteger count = new AtomicInteger(0);
-
+        
         Loop.withIndex(0, mainChannelXYdata.length, new Loop.BodyWithIndex() {
             @Override
             public void run(int i) {
                 try {
-                    double[] counts = calcNeighborCount(mainChannelXYdata[i], mainChannelTree, squaredRadiusDomain);
+                    double[] counts = calcNeighborCount(mainChannelXYdata[i], mainChannelTree, squaredRadiusDomainPx);
                     for(int j = 0; j < counts.length; j++) {
-                        counts[j] = counts[j] / counts[lastRadiusIndex] * maxSquaredRadius / squaredRadiusDomain[j];
+                        counts[j] = counts[j] / counts[lastRadiusIndex] * maxSquaredRadius / squaredRadiusDomainPx[j];
                     }
 
-                    double[] counts2 = calcNeighborCount(mainChannelXYdata[i], otherChannelTree, squaredRadiusDomain);
+                    double[] counts2 = calcNeighborCount(mainChannelXYdata[i], otherChannelTree, squaredRadiusDomainPx);
+                    nearestNeighborDistances[i] = getDistanceToNearestNeighbor(mainChannelXYdata[i], otherChannelTree);
                     double maxCount = counts2[lastRadiusIndex];
 
                     for(int j = 0; j < counts2.length; j++) {
+                        neighborsInDistance[j][i] = counts2[j];
                         if(maxCount == 0) {
                             counts2[j] = 0;
                         } else {
-                            counts2[j] = counts2[j] / maxCount * maxSquaredRadius / squaredRadiusDomain[j];
+                            counts2[j] = counts2[j] / maxCount * maxSquaredRadius / squaredRadiusDomainPx[j];
                         }
                     }
 
@@ -109,18 +121,27 @@ public class CBC {
         double[] result = new double[squaredRadiusValues.length];
         for(KDTree.DistAndValue<double[]> neighbor : neighbors) {
             double distance = neighbor.dist;
-            int bin = (int) Math.floor(distance / radiusStep);
+            int bin = (int) Math.floor(distance / radiusStepPx);
             result[bin]++;
         }
 
         VectorMath.cumulativeSum(result, false);
         return result;
     }
+    
+    private double getDistanceToNearestNeighbor(double[] queryPoint, KDTree<double[]> kdtree) {
+        assert queryPoint != null;
+        assert kdtree != null;
+
+        double[] nn = kdtree.nearest(queryPoint);
+        return MathProxy.sqrt(MathProxy.sqr(nn[0]-queryPoint[0])+MathProxy.sqr(nn[1]-queryPoint[1]));
+    }
 
     private void fillRadiusDomain() {
-        squaredRadiusDomain = new double[radiusCount];
+        squaredRadiusDomainPx = new double[radiusCount];
+        squaredRadiusDomainNm = new double[radiusCount];
         for(int i = 0; i < radiusCount; i++) {
-            squaredRadiusDomain[i] = MathProxy.sqr((i + 1) * radiusStep);
+            squaredRadiusDomainPx[i] = MathProxy.sqr((i + 1) * radiusStepPx);
         }
     }
 
