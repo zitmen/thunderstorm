73f5fe30076114d2c1fe3454b20d17392bfb43b0
Martin Ovesny <zitmen@gmail.com>
2015-10-04 05:05:33 +0200

    updated help: localization uncertainty

--- src/main/java/cz/cuni/lf1/lge/ThunderSTORM/ThunderSTORM.java
+++ src/main/java/cz/cuni/lf1/lge/ThunderSTORM/ThunderSTORM.java
@@ -2,7 +2,7 @@ package cz.cuni.lf1.lge.ThunderSTORM;
 
 public class ThunderSTORM {
     
-    public static final String VERSION = "dev-2015-08-02-b1";
+    public static final String VERSION = "dev-2015-10-03-b1";
     public static final String URL_DAILY = "https://googledrive.com/host/0BzOGc-xMFyDYR1JaelZYQmJsaUE/builds/daily";
     public static final String URL_STABLE = "https://googledrive.com/host/0BzOGc-xMFyDYR1JaelZYQmJsaUE/builds/stable";
     public static final String FILE_NAME = "Thunder_STORM";
