8af7f667feb434acddfdef4796303f5176558345
Martin Ovesny <zitmen@gmail.com>
2016-07-24 21:40:26 +0200

    all dependencies resolved by maven + TS version is read from maven properties + travis ci build badge

--- src/main/java/cz/cuni/lf1/lge/ThunderSTORM/ThunderSTORM.java
+++ src/main/java/cz/cuni/lf1/lge/ThunderSTORM/ThunderSTORM.java
@@ -1,10 +1,25 @@
 package cz.cuni.lf1.lge.ThunderSTORM;
 
+import ij.IJ;
+
+import java.io.IOException;
+import java.util.Properties;
+
 public class ThunderSTORM {
     
-    public static final String VERSION = "dev-2016-07-20-b1";
     public static final String URL_DAILY = "https://googledrive.com/host/0BzOGc-xMFyDYR1JaelZYQmJsaUE/builds/daily";
     public static final String URL_STABLE = "https://googledrive.com/host/0BzOGc-xMFyDYR1JaelZYQmJsaUE/builds/stable";
     public static final String FILE_NAME = "Thunder_STORM";
+
+    public static final String VERSION;
+    static {
+        try {
+            Properties p = new Properties();
+            p.load(IJ.getClassLoader().getResourceAsStream("thunderstorm.properties"));
+            VERSION = p.getProperty("version");
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read build properties! The package is probably corrupted.");
+        }
+    }
 
 }
