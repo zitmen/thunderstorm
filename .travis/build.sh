81097753e362f8be635255874579d39b8952af71
Hadrien Mary <hadrien.mary@gmail.com>
2018-09-05 18:13:16 -0400

    Prepare auto upload

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh  
+sh travis-build.sh
+#curl -fsLO https://raw.githubusercontent.com/fiji/fiji/master/bin/upload-update-site.sh
+#sh upload-update-site.sh
